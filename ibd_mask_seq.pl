1619e287898f479c87b2e238cd67d3fe0ddc7327
justin-lack <justin-lack@users.noreply.github.com>
2014-12-27 23:46:26 -0600

    Create ibd_mask_seq.pl

---
+++ ibd_mask_seq.pl
@@ -0,0 +1,96 @@
+#!/usr/bin/perl -w
+use strict;
+use List::Util 'shuffle';
+
+#INPUT
+
+my @line = ();
+my @FilterLine = ();
+my @Chrom = ();
+my @Start = ();
+my @Stop = ();
+my $cmd = '';
+my @Sequence = ();
+my @AllFiles = ();
+my @SeqFiles = ();
+
+#open directory
+opendir DIR, "." or die "couldn't open directory\n";
+@AllFiles = readdir(DIR);
+closedir DIR;
+
+#find correct input files
+for ($a = 0; $a < @AllFiles; $a++){
+  if ($AllFiles[$a] =~ m'.seq'){
+    push @SeqFiles, $AllFiles[$a];
+  }
+}
+
+$cmd = 'mkdir unmasked_ibd';
+system($cmd);
+
+open X, "<ibd_filter_tracts.txt";
+while (<X>){
+  chomp;
+  last if m/^$/;
+  @line = split;
+  next if ($line[0] =~ m/#/);
+  if ($line[0] ne '#'){
+    push @FilterLine, $line[0];
+    push @Chrom, $line[1];
+    push @Start, $line[2];
+    push @Stop, $line[3];
+  }
+}
+
+close X;
+undef @line;
+my $i = 0;
+my $c = 0;
+my $d = 0;
+my $hold = '';
+my $outfile = '';
+my $infile = '';
+my @filebank = 'null';
+
+for ($d = 0; $d < @SeqFiles; $d++) {
+  for ($i = 0; $i <@Chrom; $i++) {
+    if (($SeqFiles[$d] =~ m/$Chrom[$i]/) && ($SeqFiles[$d] =~ m/$FilterLine[$i]/)) {
+      $outfile = $FilterLine[$i] . '_' . $Chrom[$i] . '.seq';
+      $infile = $FilterLine[$i] . '_' . $Chrom[$i] . '.seq';
+      open Y, "<$infile";
+      while (<Y>){
+	chomp;
+	last if m/^$/;
+	@line = split;
+	next if ($line[0] =~ m/#/);
+	if ($line[0] ne '3'){
+	  push @Sequence, (split //, $line[0]);
+	}
+      }
+      $cmd = 'mv -n ' . $infile . ' unmasked_ibd';
+      system($cmd);
+      open C, ">$outfile";
+      my $a = 0;
+      for ($a = 0; $a < @Sequence; $a++) {
+	if (($a >= ($Start[$i] - 100001)) && ($a <= ($Stop[$i] + 100001))) {
+	  print C 'N';
+	}
+	else {
+	  if ($a == ($Stop[$i] + 100002)) {
+	    print C "$Sequence[$a]";
+	  }
+	  else {
+	    print C "$Sequence[$a]";
+	  }
+	}
+      }
+      undef @Sequence;
+      close C;
+      close Y;
+      undef @line;
+      $cmd = 'purge';
+      system($cmd);
+    }
+  }
+}
